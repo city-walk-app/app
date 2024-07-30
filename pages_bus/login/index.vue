b0ff64bcb92997bbc0d97a26623259c4b2cedde5
Tyh2001 <1469442737@qq.com>
2024-07-30 17:49:18 +0800

    feat:

--- pages_bus/login/index.vue
+++ pages_bus/login/index.vue
@@ -9,315 +9,261 @@
 
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
 
-  /** 登录按钮禁用状态 */
-  const disabled = ref(false)
-  /** 登陆信息 */
-  const loginInfo = reactive({ email: '', code: '' })
   /** 步骤 */
   const step = ref(0)
-  /** 用户身份信息 */
-  const userInfo = ref()
-  /** 按钮倒计时 */
-  const timmer = ref(60)
-  /**获取验证码按钮禁用状态 */
-  const getCodeButtonLoading = ref(false)
-  /** 倒计时定时器 */
-  let timeout
 
-  /** 验证码长度 */
-  // const codeDigits = 6
-  /** 验证码聚焦索引 */
-  const activeCodeIndex = ref(0)
+  // setTimeout(() => {
+  //   step.value += 1
+  // }, 2000)
 
-  /** 当前需要聚焦的文本框索引 */
-  const inputFouksIndex = ref(0)
-  /** 验证码输入框是否聚焦 */
-  const codeInputFouks = ref(false)
+  // /** 登录按钮禁用状态 */
+  // const disabled = ref(false)
+  // /** 登陆信息 */
+  // const loginForm = reactive({ email: '', code: '' })
 
-  // 监听再验证码输入页面返回之后聚焦验证码输入框
-  onShow(() => {
-    if (step.value === 1) {
-      codeInputFouks.value = true
-    }
-  })
+  // /** 用户身份信息 */
+  // const userInfo = ref()
+  // /** 按钮倒计时 */
+  // const timmer = ref(60)
+  // /**获取验证码按钮禁用状态 */
+  // const getCodeButtonLoading = ref(false)
+  // /** 倒计时定时器 */
+  // let timeout
+
+  // /** 验证码长度 */
+  // // const codeDigits = 6
+  // /** 验证码聚焦索引 */
+  // const activeCodeIndex = ref(0)
+
+  // /** 当前需要聚焦的文本框索引 */
+  // const inputFouksIndex = ref(0)
+  // /** 验证码输入框是否聚焦 */
+  // const codeInputFouks = ref(false)
+
+  // // 监听再验证码输入页面返回之后聚焦验证码输入框
+  // onShow(() => {
+  //   if (step.value === 1) {
+  //     codeInputFouks.value = true
+  //   }
+  // })
 
   /**
    * 发送验证码
    */
   const sendEmail = async () => {
-    // loading 状态下直接返回
-    if (getCodeButtonLoading.value) {
-      return
-    }
-
     // 邮箱不正确
-    if (!emailRegex.test(loginInfo.email)) {
+    if (!emailRegex.test(loginForm.email)) {
       toast('输入正确邮箱之后再获取')
       return
     }
 
     showLoading('获取中...')
 
-    const res = await API.sendEmail({ email: loginInfo.email })
+    const res = await API.sendEmail({ email: loginForm.email })
 
     hideLoading()
-    toast(res.message)
-
-    getCodeButtonLoading.value = true
 
     if (res.code === 200) {
-      startTimmer()
+      toast('获取成功')
       step.value = 1
-      return
-    }
-
-    clearInterval(timeout)
-    getCodeButtonLoading.value = false
-    timmer.value = 60
-  }
-
-  /**
-   * 开始按钮倒计时
-   */
-  const startTimmer = () => {
-    timeout = setInterval(() => {
-      if (timmer.value === 0) {
-        clearInterval(timeout)
-        getCodeButtonLoading.value = false
-        timmer.value = 60
-        return
-      }
-
-      timmer.value--
-    }, 1000)
-  }
-
-  /**
-   * 登录
-   */
-  const onLogin = async () => {
-    // 邮箱不正确
-    if (!emailRegex.test(loginInfo.email)) {
-      toast('邮箱格式不正确')
-      return
-    }
-
-    // 验证码验证失败
-    if (!loginInfo.code || loginInfo.code.length !== 6) {
-      toast('验证码格式不正确')
-      return
-    }
-
-    showLoading('处理中...')
-
-    disabled.value = true
-
-    const res = await API.emailLogin(loginInfo)
-
-    hideLoading()
-
-    disabled.value = false
-
-    if (res.code === 200) {
-      disabled.value = false
-
-      userInfo.value = res.data
-      setStorage(USER_INFO, res.data.user_info)
-      setStorage(USER_TOKEN, res.data.token)
-
-      uni.redirectTo({ url: '/pages/home/index' })
-      return
-    }
-
-    toast(res.message)
-  }
-
-  /**
-   * 监听验证码
-   */
-  watch(
-    () => loginInfo.code,
-    (newVal, oldVal) => {
-      if (newVal.length === 6) {
-        onLogin()
-        return
-      }
-
-      if (newVal.length === 0) {
-        activeCodeIndex.value = 0
-        return
-      }
-
-      if (newVal.length > oldVal.length) {
-        activeCodeIndex.value = newVal.length
-      } else if (newVal.length < oldVal.length) {
-        activeCodeIndex.value = newVal.length - 1
-      }
-    }
-  )
-
-  const wxLogin = async () => {
-    const res = await uni.login()
-
-    console.log(res)
-  }
-
-  wxLogin()
-
-  /**
-   * 手机号登录
-   */
-  const getphonenumber = async (evt) => {
-    console.log('获取手机号', evt)
-    if (evt.detail.errMsg === 'getPhoneNumber:ok') {
-      const mobileCode = evt.detail.code
-
-      const loginRes = await uni.login()
-
-      const res = await API.wechatLogin({
-        mobile_code: mobileCode,
-        open_id_code: loginRes.code,
-      })
-
-      if (res.code === 200) {
-        console.log(res)
-      }
     }
   }
+
+  // /**
+  //  * 发送验证码
+  //  */
+  // const sendEmail = async () => {
+  //   // loading 状态下直接返回
+  //   if (getCodeButtonLoading.value) {
+  //     return
+  //   }
+
+  //   // 邮箱不正确
+  //   if (!emailRegex.test(loginForm.email)) {
+  //     toast('输入正确邮箱之后再获取')
+  //     return
+  //   }
+
+  //   showLoading('获取中...')
+
+  //   const res = await API.sendEmail({ email: loginForm.email })
+
+  //   hideLoading()
+  //   toast(res.message)
+
+  //   getCodeButtonLoading.value = true
+
+  //   if (res.code === 200) {
+  //     startTimmer()
+  //     step.value = 1
+  //     return
+  //   }
+
+  //   clearInterval(timeout)
+  //   getCodeButtonLoading.value = false
+  //   timmer.value = 60
+  // }
+
+  // /**
+  //  * 开始按钮倒计时
+  //  */
+  // const startTimmer = () => {
+  //   timeout = setInterval(() => {
+  //     if (timmer.value === 0) {
+  //       clearInterval(timeout)
+  //       getCodeButtonLoading.value = false
+  //       timmer.value = 60
+  //       return
+  //     }
+
+  //     timmer.value--
+  //   }, 1000)
+  // }
+
+  // /**
+  //  * 登录
+  //  */
+  // const onLogin = async () => {
+  //   // 邮箱不正确
+  //   if (!emailRegex.test(loginForm.email)) {
+  //     toast('邮箱格式不正确')
+  //     return
+  //   }
+
+  //   // 验证码验证失败
+  //   if (!loginForm.code || loginForm.code.length !== 6) {
+  //     toast('验证码格式不正确')
+  //     return
+  //   }
+
+  //   showLoading('处理中...')
+
+  //   disabled.value = true
+
+  //   const res = await API.emailLogin(loginForm)
+
+  //   hideLoading()
+
+  //   disabled.value = false
+
+  //   if (res.code === 200) {
+  //     disabled.value = false
+
+  //     userInfo.value = res.data
+  //     setStorage(USER_INFO, res.data.user_info)
+  //     setStorage(USER_TOKEN, res.data.token)
+
+  //     uni.redirectTo({ url: '/pages/home/index' })
+  //     return
+  //   }
+
+  //   toast(res.message)
+  // }
+
+  // /**
+  //  * 监听验证码
+  //  */
+  // watch(
+  //   () => loginForm.code,
+  //   (newVal, oldVal) => {
+  //     if (newVal.length === 6) {
+  //       onLogin()
+  //       return
+  //     }
+
+  //     if (newVal.length === 0) {
+  //       activeCodeIndex.value = 0
+  //       return
+  //     }
+
+  //     if (newVal.length > oldVal.length) {
+  //       activeCodeIndex.value = newVal.length
+  //     } else if (newVal.length < oldVal.length) {
+  //       activeCodeIndex.value = newVal.length - 1
+  //     }
+  //   }
+  // )
 
   /**
    * 监听动画结束事件
    */
   const transitionend = () => {
-    inputFouksIndex.value = step.value
-
-    if (step.value === 1) {
-      codeInputFouks.value = true
-    }
+    // inputFouksIndex.value = step.value
+    // if (step.value === 1) {
+    //   codeInputFouks.value = true
+    // }
   }
 
-  /**
-   * 验证码文本框失焦
-   */
-  const onBlur = () => {
-    codeInputFouks.value = false
-  }
+  // /**
+  //  * 验证码文本框失焦
+  //  */
+  // const onBlur = () => {
+  //   codeInputFouks.value = false
+  // }
 </script>
 
 <template>
   <div class="login">
     <div
-      class="swiper-container"
+      class="login-swiper"
       :style="{ transform: `translateX(-${step * 100}vw)` }"
       @transitionend.self="transitionend"
     >
       <!-- 1. 输入邮箱 -->
-      <div class="swiper-item">
+      <div class="login-swiper-item">
+        <!-- 头部 -->
         <div class="header">
-          <image
-            class="header-logo"
-            src="https://img.erpweb.eu.org/imgs/2024/01/245c8a7130242d47.png"
-          />
-
-          <h2 class="header-title">Login</h2>
-        </div>
-
-        <button open-type="getPhoneNumber" @getphonenumber="getphonenumber">
-          快捷登录
-        </button>
-
-        <div class="login-form">
-          <div class="input-wrapper">
-            <input
-              class="login-input"
-              placeholder-class="input-placeholder"
-              type="email"
-              :adjust-position="false"
-              :focus="inputFouksIndex === 0"
-              v-model="loginInfo.email"
-              placeholder="email"
-              @confirm="sendEmail"
+          <div class="header-logo-wrapper">
+            <image
+              class="header-logo"
+              src="https://city-walk.oss-cn-beijing.aliyuncs.com/assets/images/city-walk/logo.jpg"
             />
-            <!-- @input="emailInput" -->
           </div>
 
-          <!-- 邮箱联想建议 -->
-          <!-- <div v-if="showAssociateEmail" class="email-associate">
-            <div
-              v-for="(item, index) in emailAssociate"
-              class="associate-item"
-              :key="index"
-              @click="setAssociateEmail(loginInfo.email + item)"
-            >
-              {{ loginInfo.email }}{{ item }}
-            </div>
-          </div> -->
+          <div class="header-title">欢迎，请登录</div>
         </div>
+
+        <!-- 输入框 -->
+        <div class="email-input-wrapper">
+          <input
+            v-model="loginForm.email"
+            class="email-input"
+            type="text"
+            placeholder="请输入邮箱"
+          />
+        </div>
+
+        <!-- 登录按钮 -->
+        <div class="button" @click="sendEmail"></div>
       </div>
 
       <!-- 2. 提交验证码 -->
-      <div class="swiper-item">
-        <div class="header" />
+      <div class="login-swiper-item">2</div>
 
-        <!-- <div class="code-input-wrapper" @click="codeInputClick">
-          <div
-            v-for="(item, index) in codeDigits"
-            :key="index"
-            :class="[
-              'code-input',
-              {
-                'code-input-active':
-                  activeCodeIndex === index && codeInputFouks,
-              },
-            ]"
-          >
-            {{ loginInfo.code[index] || '' }}
-          </div> -->
-
-        <input
-          class="input"
-          type="number"
-          v-model="loginInfo.code"
-          maxlength="6"
-          :adjust-position="false"
-          :focus="codeInputFouks"
-          @confirm="onLogin"
-          @blur="onBlur"
-        />
-        <!-- </div> -->
-
-        <div class="get-code-button" @click="sendEmail">
-          <template v-if="getCodeButtonLoading">{{ timmer }}s 后再试</template>
-          <template v-else>重新获取</template>
-        </div>
-      </div>
+      <!-- 3. 选择偏好 -->
+      <div class="login-swiper-item">3</div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
   .login {
-    padding-top: 100px;
-    box-sizing: border-box;
     overflow: hidden;
     height: 100vh;
-
-    .input {
-      border: 1px solid #333;
-    }
+    width: 100vw;
 
     // 登录表单轮播图
-    .swiper-container {
-      height: 80vh;
-      width: 400vw;
+    .login-swiper {
+      padding-top: 316rpx;
+      box-sizing: border-box;
+      height: inherit;
+      width: 300vw;
       display: flex;
       flex-wrap: nowrap;
-      overflow: auto;
       transition: transform 0.4s cubic-bezier(0.4, 0, 0.2, 1);
-      overflow: hidden;
 
       // 每一项轮播内容
-      .swiper-item {
+      .login-swiper-item {
         width: 100vw;
         display: flex;
         flex-direction: column;
@@ -327,147 +273,61 @@
         flex-shrink: 0;
         position: relative;
 
-        // 头部内容
+        // 头部
         .header {
           display: flex;
-          flex-direction: column;
-          justify-content: space-around;
-          align-items: center;
-          height: 150px;
+          align-items: flex-end;
+          column-gap: 40rpx;
           width: 100%;
+          justify-content: flex-start;
 
           // logo
-          &-logo {
-            width: 60px;
-            height: 60px;
+          .header-logo-wrapper {
+            width: 112rpx;
+            height: 112rpx;
             border-radius: 50%;
-            margin-top: 30px;
-            flex-shrink: 0;
+
+            .header-logo {
+              width: inherit;
+              height: inherit;
+              border-radius: 50%;
+            }
           }
 
           // 标题
-          &-title {
-            font-size: 22px;
-            margin: 20px 0;
+          .header-title {
+            font-weight: 400;
+            font-size: 56rpx;
+            color: #333333;
+            line-height: 66rpx;
           }
         }
 
-        // 提交按钮
-        .submit-button {
-          width: 80px;
-          height: 80px;
+        // 邮箱输入框
+        .email-input-wrapper {
+          margin-top: 88rpx;
+          width: 646rpx;
+          height: 116rpx;
+          background: #eeeeee;
+          border-radius: 40rpx;
+          margin-bottom: 102rpx;
+
+          .email-input {
+            width: inherit;
+            height: inherit;
+            padding: 30rpx 46rpx;
+            box-sizing: border-box;
+          }
+        }
+
+        .button {
+          width: 180rpx;
+          height: 180rpx;
+          background: #f3943f;
           border-radius: 50%;
           display: flex;
-          align-items: center;
           justify-content: center;
-          background-color: #2d5af1;
-          margin-top: 40px;
-
-          .city-icon {
-            font-size: 26px;
-            color: #fff;
-          }
-        }
-
-        // 登录表单
-        .login-form {
-          position: relative;
-          width: 100%;
-
-          // 文本框
-          .input-wrapper {
-            border: 1px solid #ddd;
-            background: #fff;
-            border-radius: 10px;
-            height: 44px;
-            padding: 0 20px;
-            transition: 0.3s;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            position: relative;
-
-            // 输入框
-            .login-input {
-              height: 100%;
-              width: 100%;
-              border: none;
-              font-size: 15px;
-              color: #333;
-              outline: none;
-              text-align: center;
-            }
-
-            .input-placeholder {
-              text-align: center;
-            }
-
-            &:focus-within {
-              border-color: #2d5af1;
-            }
-          }
-
-          // 邮箱联想建议
-          .email-associate {
-            margin-top: 7px;
-            width: 100%;
-            border-radius: 14px;
-            background-color: rgb(247, 247, 247);
-
-            .associate-item {
-              padding: 12px 15px;
-              box-sizing: border-box;
-              font-size: 14px;
-              color: #111;
-              font-family: PingFangSC-Semibold, PingFang SC;
-              font-weight: 300;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-            }
-          }
-        }
-
-        // 验证码输入框
-        .code-input-wrapper {
-          width: 100%;
-          display: flex;
           align-items: center;
-          justify-content: space-between;
-          overflow: hidden;
-
-          // 每一项输入框
-          .code-input {
-            width: 44px;
-            height: 44px;
-            background: #f6f6f6;
-            border-radius: 12px;
-            text-align: center;
-            border: 1px solid transparent;
-            transition: 0.3s;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-
-            &.code-input-active {
-              border-color: #2d5af1;
-            }
-          }
-
-          // 影子输入框
-          .code-input-shadow {
-            position: fixed;
-            top: -9999px;
-            opacity: 0;
-            color: transparent;
-          }
-        }
-
-        // 获取验证码按钮
-        .get-code-button {
-          margin-top: 40px;
-          color: #5a7cec;
-          font-size: 14px;
         }
       }
     }
